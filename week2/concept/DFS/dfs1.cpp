48a1ebc0d528fe806efdf5cbebc45507e1715b7d
장성원 <jangseong-won@jangseong-won-ui-MacBookPro.local>
2025-06-26 17:32:40 +0900

    인접, DFS

---
+++ week2/concept/DFS/dfs1.cpp
@@ -0,0 +1,27 @@
+//돌다리 두들겨 보기 권법
+#include<bits/stdc++.h>
+using namespace std;
+const int n = 6;
+vector<int> adj[n];
+int visited[n];
+
+void DFS(int u) {
+    visited[u] = 1;
+    cout << u << "\n";
+    for(int v : adj[u]) {
+        if(visited[v] == 0 ) {
+            DFS(v);
+        }
+    }
+    cout << u << "로부터 시작된 함수가 종료되었습니다.\n";
+    return;
+}
+
+int main() {
+    adj[1].push_back(2);
+    adj[1].push_back(3);
+    adj[2].push_back(4);
+    adj[4].push_back(2);
+    adj[2].push_back(5);
+    DFS(1);
+}
