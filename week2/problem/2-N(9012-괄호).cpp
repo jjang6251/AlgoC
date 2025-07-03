b553e9ee7608461d217d64347ea9ffe0ce296325
장성원 <jangseong-won@jangseong-won-ui-MacBookPro.local>
2025-07-03 21:35:04 +0900

    2-N

--- week2/problem/2-N(9012-괄호).cpp
+++ week2/problem/2-N(9012-괄호).cpp
@@ -42,3 +42,28 @@ int main()
     }
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int n;  
+// string s; 
+// bool check(string s){
+// 	stack<char> stk;
+// 	for(char c : s){
+// 		if(c == '(')stk.push(c); 
+// 		else{
+// 			if(!stk.empty()) stk.pop(); 
+// 			else return false; 
+// 		}
+// 	}
+// 	return stk.empty();
+// }
+// int main() {
+// 	cin >> n; 
+// 	for(int i = 0; i < n; i++){
+// 		cin >> s; 
+// 		if(check(s)) cout << "YES\n"; 
+// 		else cout << "NO\n";
+// 	}  
+// 	return 0;
+// } 
