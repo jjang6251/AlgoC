91af9d4f50400449b37d260d6adf0544214b1893
장성원 <jjang6251@gmail.com>
2025-10-02 19:39:44 +0900

    5-A

---
+++ week5/problem/5-A(2109-순회강연).cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int n, a, b, ret; 
+vector<pair<int, int>> v; 
+priority_queue<int, vector<int>, greater<int>> pq;
+int main(){
+    cin >> n; 
+    for(int i = 0; i < n; i++){
+        cin >> a >> b; v.push_back({b, a});
+    }
+    sort(v.begin(), v.end());
+    for(int i = 0; i < n; i++){ 
+        pq.push(v[i].second);
+        if(pq.size() > v[i].first){ 
+            pq.pop();
+        } 
+    } 
+    while(pq.size()){
+    	ret += pq.top(); pq.pop();
+	}
+    cout << ret << "\n";
+}
