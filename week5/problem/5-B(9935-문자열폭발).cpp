590a404bb0e8b273572977b0b36a2927e3c4d8cf
장성원 <jjang6251@gmail.com>
2025-10-03 19:54:03 +0900

    5-B

---
+++ week5/problem/5-B(9935-문자열폭발).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+string S, T, ret;
+int main() {
+    cin >> S >> T;
+    for(char a : S){
+        ret += a;
+        if(ret.size() >= T.size() && ret.substr(ret.size() - T.size(), T.size()) == T){
+            ret.erase(ret.end() - T.size(), ret.end()); 
+        }
+    }
+    if(!ret.size())cout << "FRULA" << "\n";
+    else cout << ret << "\n";
+    return 0;
+}
