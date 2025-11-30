c7b9e066e867ccf8469ed7d90c6c6546d14bc2b3
JjangSW <jjang6251@gmail.com>
2025-11-30 16:54:38 +0900

    5-C(1781-컵라면)

---
+++ week5/problem/5-C(1781-컵라면).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int n, a, b, ret; 
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq; 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	cin >> n; 
+	for(int i = 0; i < n; i++){
+		cin >> a >> b; 
+		v.push_back({a, b}); 
+	}
+	sort(v.begin(), v.end()); 
+	for(int i = 0; i < v.size(); i++){ 
+		ret += v[i].second; 
+		pq.push(v[i].second); 
+		if(pq.size() > v[i].first){
+			ret -= pq.top();  
+			pq.pop(); 
+		}
+	}
+	cout << ret << "\n";  
+    return 0;
+}
