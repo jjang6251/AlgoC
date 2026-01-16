4c485a274129c5b296a0bc0d2148dc9c9304b332
JjangSW <jjang6251@gmail.com>
2026-01-16 13:44:55 +0900

    5-F

---
+++ week5/problem/5-F(1202-보석도둑).cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n, k, ret;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> k;
+
+    vector<pair<ll,ll>> v(n);
+    vector<ll> vv(k);
+
+    for(int i = 0; i < n; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+    for(int i = 0; i < k; i++) {
+        cin >> vv[i];
+    }
+
+    sort(v.begin(), v.end());
+    sort(vv.begin(), vv.end());
+
+    priority_queue<int> pq;
+    int j = 0;
+    for(int i = 0; i < k; i++) {
+        while(j < n && v[j].first <= vv[i]) pq.push(v[j++].second);
+        if(pq.size()) {
+            ret += pq.top(); pq.pop();
+        }
+    }
+
+    cout << ret << "\n";
+    return 0;
+}
